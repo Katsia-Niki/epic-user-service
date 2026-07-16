b14c02e1665e0620cebf76d56b6f6c00eeaa07d4
Katsia-Niki <katsiaryna.niki@gmail.com>
2026-07-16 16:27:45 +0300

    initial project structure

---
+++ src/main/java/by/nikiforova/userservice/EpicUserServiceApplication.java
@@ -0,0 +1,13 @@
+package by.nikiforova.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/nikiforova/userservice/EpicUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nikiforova.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
